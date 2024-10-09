36471674736e0bb6b8f1af917a3a888e77ab0e97
Celeste <93024079+celeste-melissa@users.noreply.github.com>
2024-10-09 18:34:45 -0400

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,13 +1,13 @@
-
 # main function
 def main():
     # ask the user for the answer to the Great Question
-    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+
     if correct_answer(answer):
         print("Yes")
     else:
         print("No")
-# function will evaluate the answer using a single line of or statements 
+# function will evaluate the answer using if statements
 def correct_answer(a):
     return True if a == "42" or a == "forty two" or a == "forty-two" else False
 main()
